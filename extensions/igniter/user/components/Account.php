8d9b4fa6cebdc37a664484077ee55c7f955fe665
pankajkmehta <pankajm.softprodigy@gmail.com>
2019-11-25 12:47:56 +0530

    Telephone field validation

--- extensions/igniter/user/components/Account.php
+++ extensions/igniter/user/components/Account.php
@@ -210,7 +210,7 @@ class Account extends \System\Classes\BaseComponent
                 ['email', 'lang:igniter.user::default.settings.label_email', 'required|email|unique:customers,email'],
                 ['password', 'lang:igniter.user::default.login.label_password', 'required|min:6|max:32|same:password_confirm'],
                 ['password_confirm', 'lang:igniter.user::default.login.label_password_confirm', 'required'],
-                ['telephone', 'lang:igniter.user::default.settings.label_telephone', 'required'],
+                ['telephone', 'lang:igniter.user::default.settings.label_telephone', 'required|digits_between:10,20'],
                 ['newsletter', 'lang:igniter.user::default.login.label_subscribe', 'integer'],
             ];
 
@@ -275,7 +275,7 @@ class Account extends \System\Classes\BaseComponent
                 ['old_password', 'lang:igniter.user::default.label_email', 'sometimes'],
                 ['new_password', 'lang:igniter.user::default.label_password', 'required_with:old_password|min:6|max:32|same:confirm_new_password'],
                 ['confirm_new_password', 'lang:igniter.user::default.label_password_confirm', 'required_with:old_password'],
-                ['telephone', 'lang:igniter.user::default.label_telephone', 'required'],
+                ['telephone', 'lang:igniter.user::default.label_telephone', 'required|digits_between:10,20'],
                 ['newsletter', 'lang:igniter.user::default.login.label_subscribe', 'integer'],
             ];
 
