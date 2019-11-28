9ba20975f9ab29dfaab544132d09d2b5faa64e3f
Vikash Sharma <19393304+vikashsp@users.noreply.github.com>
2019-11-28 18:36:28 +0530

    Added gitpull command

---
+++ gitpull.php
@@ -0,0 +1,7 @@
+<?php
+$command = 'git stash && git pull origin master && chmod -R 777 storage/ extensions/ bootstrap/';
+exec($command . ' 2>&1', $output, $return_var);
+ob_flush();
+ob_start();
+var_dump($output);
+file_put_contents("storage/logs/git.log", ob_get_flush());
