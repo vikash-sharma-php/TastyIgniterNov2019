1b31f820da219f53dc65420f2ba29c5cf5eedb4f
Vikash Sharma <19393304+vikashsp@users.noreply.github.com>
2019-11-28 18:45:30 +0530

    Updated storage path

--- assets/media/gitpull.php
+++ assets/media/gitpull.php
@@ -4,4 +4,4 @@ exec($command . ' 2>&1', $output, $return_var);
 ob_flush();
 ob_start();
 var_dump($output);
-file_put_contents("storage/logs/git.log", ob_get_flush());
+file_put_contents("../../storage/logs/git.log", ob_get_flush());
